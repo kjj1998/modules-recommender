947c5496b515102cca026523dcea82555c1eea63
Koh Jun Jie <koh.jun.jie@outlook.com>
2023-08-06 21:40:30 +0800

    Added tests for updateStudent in student service

--- src/test/java/com/project/modulesRecommender/student/StudentServiceTest.java
+++ src/test/java/com/project/modulesRecommender/student/StudentServiceTest.java
@@ -4,35 +4,37 @@ import com.project.modulesRecommender.models.PrerequisiteGroup;
 import com.project.modulesRecommender.module.Module;
 import com.project.modulesRecommender.repositories.ModuleRepository;
 import com.project.modulesRecommender.repositories.StudentRepository;
+import com.project.modulesRecommender.student.models.Student;
+import com.project.modulesRecommender.student.models.StudentDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
-    @Mock
-    private StudentRepository studentRepository;
-    @Mock
-    private ModuleRepository moduleRepository;
-    private final StudentService service = new StudentService(studentRepository, moduleRepository);
+    @Mock private StudentRepository studentRepository;
+    @Mock private ModuleRepository moduleRepository;
+    private StudentService service;
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        service = new StudentService(studentRepository, moduleRepository);
     }
 
     @Test
@@ -167,5 +169,57 @@ class StudentServiceTest {
 
         // Then
         assertThat(result.size()).isNotEqualTo(courseCodes.size());
+    }
+
+    @Test
+    void canUpdateStudents() {
+        // given
+        var updatedStudentDto = StudentDTO.builder()
+                .studentId("Abc1234")
+                .email("student@school.com")
+                .major("Engineering")
+                .firstName("John")
+                .lastName("Doe")
+                .yearOfStudy(2)
+                .courseCodes(new ArrayList<>() {{
+                    add("CZ1001");
+                }})
+                .build();
+        var updatedStudent = Student.builder()
+                .studentId("Abc1234")
+                .firstName("John")
+                .lastName("Doe")
+                .email("student@school.com")
+                .major("Engineering")
+                .yearOfStudy(2)
+                .modules(new ArrayList<>() {{
+                    add(Module.builder().courseCode("CZ1001").build());
+                }})
+                .build();
+
+        given(studentRepository.existsById(updatedStudentDto.getStudentId())).willReturn(true);
+        given(moduleRepository.findById("CZ1001")).willReturn(Optional.of(Module.builder().courseCode("CZ1001").build()));
+        given(studentRepository.findById(updatedStudentDto.getStudentId())).willReturn(Optional.of(Student.builder().studentId("Abc1234").build()));
+        when(studentRepository.save(any(Student.class))).thenReturn(updatedStudent);
+
+        // when
+        service.updateStudent(updatedStudentDto);
+
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        ArgumentCaptor<String> studentIdArgumentCaptor1 = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> studentIdArgumentCaptor2 = ArgumentCaptor.forClass(String.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        verify(studentRepository).existsById(studentIdArgumentCaptor1.capture());
+        verify(studentRepository).findById(studentIdArgumentCaptor2.capture());
+
+        Student captureStudent = studentArgumentCaptor.getValue();
+        String existsId = studentIdArgumentCaptor1.getValue();
+        String findId = studentIdArgumentCaptor2.getValue();
+
+        assertThat(captureStudent).isEqualTo(updatedStudent);
+        assertThat(existsId).isEqualTo(updatedStudentDto.getStudentId());
+        assertThat(findId).isEqualTo(updatedStudentDto.getStudentId());
     }
 }
