5954941c653cabdf156ea1154780e322d304c7e8
Koh Jun Jie <koh.jun.jie@outlook.com>
2023-08-18 10:11:38 +0800

    completed pagination feature for the page that displays all the modules

--- frontend/app/modules/[page]/page.tsx
+++ frontend/app/modules/[page]/page.tsx
@@ -16,30 +16,40 @@ export default async function page({ params: { page } }: Props) {
   const modules = await getModules((pageNum - 1) * 10, 10)
   
   const totalNumOfModules = modules?.at(0)?.total ?? 0
-  const totalNumOfPages = Math.floor(totalNumOfModules / 10) + (totalNumOfModules % 10)
+  const totalNumOfPages = Math.floor(totalNumOfModules / 10) + (totalNumOfModules % 10 !== 0 ? 1 : 0)
 
   const pageNumbers = [];
+  pageNumbers.push(pageNum)
 
   if (pageNum > 5) {
-    for (let i = 1; i <= 4; i++) {
-      pageNumbers.push(pageNum - i);
-      pageNumbers.push(pageNum + i);
+    if (totalNumOfPages - pageNum < 4) {
+      for (let i = 1; i <= 9 - (totalNumOfPages - pageNum + 1); i++) {
+        pageNumbers.push(pageNum - i)
+      }
+    } else {
+      for (let i = 1; i <= 4; i++) {
+        pageNumbers.push(pageNum - i)
+      }
     }
-  
-    pageNumbers.push(pageNum)
-    pageNumbers.sort((a, b) => a - b);
-
   } else {
-    for (let i = 1; i <= 9; i++) {
+    for (let i = 1; i < pageNum; i++) {
       pageNumbers.push(i);
     }
   }
   
-  // for (let i = 1; i <= totalNumOfPages; i++) {
-  //   if (i > 10) 
-  //     break
-  //   pageNumbers.push(i);
-  // }
+  if (pageNum <= totalNumOfPages - 4) {
+    const curPagesForDisplay = pageNumbers.length
+
+    for (let i = 1; i <= 9 - curPagesForDisplay; i++) {
+      pageNumbers.push(pageNum + i);
+    }
+  } else {
+    for (let i = pageNum + 1; i <= totalNumOfPages; i++) {
+      pageNumbers.push(i);
+    }
+  }
+
+  pageNumbers.sort((a, b) => a - b);
 
   return (
     <main className="mx-auto max-w-5xl py-1 min-h-screen mt-16">
@@ -52,6 +62,24 @@ export default async function page({ params: { page } }: Props) {
       </ul>
       <nav>
         <ul className='flex mb-4 justify-center items-center'>
+          {
+            pageNum !== 1 ? (
+              <li className='p-1 mx-1'>
+                <Link href={`/modules/${1}`}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="block w-5"><polyline points="11 17 6 12 11 7"></polyline><polyline points="18 17 13 12 18 7"></polyline></svg>
+                </Link>
+              </li>
+            ) : <div className='hidden'></div>
+          }
+          {
+            pageNum !== 1 ? (
+              <li className='p-1 mx-1'>
+                <Link href={`/modules/${pageNum - 1}`}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="block w-5"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                </Link>
+              </li>
+            ) : <div className='hidden'></div>
+          }
           {
             pageNumbers.map((pageNumber) => {
               return (
@@ -66,13 +94,21 @@ export default async function page({ params: { page } }: Props) {
               )
             })
           }
-          <li className='p-2 mx-1'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="block w-5"><polyline points="9 18 15 12 9 6"></polyline></svg>
-          </li>
           {
-            totalNumOfPages > 10 ? (
-              <li className=''> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="block w-5"><polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline></svg>
+            pageNum !== totalNumOfPages ? (
+              <li className='p-1 mx-1'>
+                <Link href={`/modules/${pageNum + 1}`}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="block w-5"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                </Link>
+              </li>
+            ) : <div className='hidden'></div>
+          }
+          {
+            totalNumOfPages > 9 && pageNum !== totalNumOfPages ? (
+              <li className='p-1 mx-1'>
+                <Link href={`/modules/${totalNumOfPages}`}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="block w-5"><polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline></svg>
+                </Link>
               </li>
             ) : <div className='hidden'></div>
           }
